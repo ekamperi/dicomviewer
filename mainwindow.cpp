b11b813cb762ad8f5fc8132430e1786d353ea286
Stathis Kamperis <ekamperi@gmail.com>
2014-07-26 14:54:10 +0300

    Add rudimentary code to read a DICOM file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+/* XXX: This is needed for compile */
+#define HAVE_CONFIG_H
+
+#include "dcmtk/config/osconfig.h"
+#include "dcmtk/dcmdata/dctk.h"
+
+#include <iostream>
 #include <QFileDialog>
 #include <QDebug>
 
@@ -23,4 +30,19 @@ void MainWindow::on_actionAbout_triggered()
 void MainWindow::on_actionOpenDICOM_triggered()
 {
     QString filename = QFileDialog::getOpenFileName(this);
+
+    DcmFileFormat dcmFile;
+    OFCondition status = dcmFile.loadFile(filename.toStdString().c_str());
+    if (status.good()) {
+        OFString patientName;
+        status = dcmFile.getDataset()->findAndGetOFString(
+                    DCM_PatientName, patientName);
+        if (status.good()) {
+            qDebug() << "Patient name:" << patientName.c_str();
+        } else {
+            qWarning() << status.text();
+        }
+    } else {
+        qWarning() << status.text();
+    }
 }
